531fd61a4c3cef41137d5b9b65b4c4bc3ce7f14e
Dfletch <plebeianpony@gmail.com>
2023-01-08 15:30:55 -0600

    fixed server routing

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const path = require('path');
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
+require('./routes/apiRoute')(app);
+require('./routes/htmlRoute')(app);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
